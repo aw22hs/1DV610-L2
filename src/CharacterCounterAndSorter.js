79202ac4e999ee2583ae27091078c53e9a5d1e86
Anja Willsund <aw22hs@student.lnu.se>
2023-10-24 17:20:04 +0200

    Add class CharacterCounterAndSorter

---
+++ src/CharacterCounterAndSorter.js
@@ -0,0 +1,58 @@
+/**
+ * Module for counting and sorting characters.
+ *
+ * @author Anja Willsund <aw22hs@student.lnu.se>
+ * @version 1.0.0
+ */
+
+export class CharacterCounterAndSorter {
+  #sortedCharacters
+
+  /**
+   * Initializes a new instance of the CharacterCounterAndSorter class.
+   *
+   * @param {string[]} characters - The text input.
+   */
+  constructor(characters) {
+    this.#countAndSortInAlphabeticalOrder(characters)
+  }
+
+  #countAndSortInAlphabeticalOrder(characters) {
+    const characterCount = this.#countCharacters(characters)
+    this.#sortedCharacters = this.#sortCharactersAlphabetically(characterCount)
+  }
+
+  #countCharacters(characters) {
+    if (!characters) {
+      return {}
+    }
+    // Characters can be either letters or words
+    const characterCount = {}
+    for (const character of characters) {
+      if (character === '') {
+        return
+      }
+      if (characterCount[character] === undefined) {
+        characterCount[character] = 1
+      } else {
+        characterCount[character] += 1
+      }
+    }
+
+    return characterCount
+  }
+
+  #sortCharactersAlphabetically(characters) {
+    const sortedCharacters = {}
+    // Sorts the 'characterCount' object in alphabetical order
+    Object.keys(characters).sort().forEach(key => {
+      sortedCharacters[key] = characters[key]
+    })
+
+    return sortedCharacters
+  }
+
+  getSortedCharacters() {
+    return this.#sortedCharacters
+  }
+}
