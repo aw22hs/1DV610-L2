7d7d0f17225d79c15d3d4d03d640e159cc7795fa
Anja Willsund <aw22hs@student.lnu.se>
2023-10-24 12:26:10 +0200

    Add replaceLowerCaseAndCapitalizedWord(), replaceWordsWithExactFormatting() and getWordInCapitalizedAndLowerCaseFormat()

--- src/UpdatedTextAnalyzer.js
+++ src/UpdatedTextAnalyzer.js
@@ -1,9 +1,11 @@
 /**
- * Module for analyzing the updated text..
+ * Module for analyzing the updated text.
  *
  * @author Anja Willsund <aw22hs@student.lnu.se>
  * @version 1.0.0
  */
+
+import { WordValidator } from "./WordValidator.js"
 
 export class UpdatedTextAnalyzer {
   #originalText = ''
@@ -62,13 +64,74 @@ export class UpdatedTextAnalyzer {
   }
 
   /**
-   * Replaces words with exact matching in the updated text.
+   * Replaces words with exact format matching in the updated text.
    *
    * @param {string} wordToReplace - The word to be replaced.
    * @param {string} newWord - The new word.
    * @returns The updated text with words replaced.
    */
-  replaceWords(wordToReplace, newWord) {
+  replaceWordsWithExactFormatting(wordToReplace, newWord) {
+    new WordValidator(wordToReplace)
+    new WordValidator(newWord)
+
+    const updatedText = this.#replaceWords(wordToReplace, newWord)
+
+    return updatedText
+  }
+
+  /**
+   * Replaces a word with a new word and returns the updated text.
+   * Replaces all words that has all letters in lower case and all words with
+   * the first letter in upper case and the rest of the letters in lower case.
+   *
+   * @param {string} wordToReplace - The word to be replaced.
+   * @param {string} newWord - The word to replace with.
+   * @returns {string} - The new text.
+   * @throws {Error} - If the word to replace does not match the correct format.
+   */
+  replaceLowerCaseAndCapitalizedWord(wordToReplace, newWord) {
+    new WordValidator(wordToReplace)
+    new WordValidator(newWord)
+
+    const wordsToReplace =
+      this.#getWordInCapitalizedAndLowerCaseFormat(wordToReplace)
+
+    let wordToReplaceMatchesCorrectFormat = false
+    for (const word of wordsToReplace) {
+      if (word === wordToReplace) {
+        wordToReplaceMatchesCorrectFormat = true
+      }
+    }
+
+    // TODO: Ska denna metod kasta ett fel eller hantera detta på annat sätt?
+    if (!wordToReplaceMatchesCorrectFormat) {
+      throw new Error('The word to replace does not match the correct format. '
+        + 'All letters need to be lower case or the first letter needs to be '
+        + 'upper case and the rest of the letters be lower case.')
+    }
+
+    const newWords = this.#getWordInCapitalizedAndLowerCaseFormat(newWord)
+
+    let updatedText
+    for (let i = 0; i < wordsToReplace.length; i++) {
+      updatedText = this.#replaceWords(wordsToReplace[i], newWords[i])
+    }
+
+    return updatedText
+  }
+
+  #getWordInCapitalizedAndLowerCaseFormat(word) {
+    const wordWithAllLettersLowerCase = word.toLowerCase()
+    const wordWithFirstLetterUpperCase =
+      wordWithAllLettersLowerCase.charAt(0).toUpperCase()
+      + wordWithAllLettersLowerCase.slice(1)
+    const words = [wordWithAllLettersLowerCase,
+      wordWithFirstLetterUpperCase]
+
+    return words
+  }
+
+  #replaceWords(wordToReplace, newWord) {
     this.#updatedText = this.#updatedText
       .replace(new RegExp('\\b' + wordToReplace + '\\b', 'g'), newWord)
 
