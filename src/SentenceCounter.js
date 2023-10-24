596557129eb5f90af67e31bbd4eb877f49a01aa2
Anja Willsund <aw22hs@student.lnu.se>
2023-10-24 17:20:28 +0200

    Rename SentenceAnalyzer to SentenceCounter

---
+++ src/SentenceCounter.js
@@ -0,0 +1,67 @@
+/**
+ * Module for analyzing sentences.
+ *
+ * @author Anja Willsund <aw22hs@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import { CharacterCounterAndSorter } from "./CharacterCounterAndSorter.js"
+
+export class SentenceCounter {
+  #sentences = []
+
+  /**
+   * Initializes a new instance of the SentenceAnalyzer class.
+   *
+   * @param {string} text - The text input.
+   */
+  constructor(text) {
+    this.#getSentencesFromText(text)
+    this.#trimSentencesFromWhitespace()
+  }
+
+  #getSentencesFromText(text) {
+    this.#sentences = text.split(/[.!?]\s+/)
+  }
+
+  #trimSentencesFromWhitespace() {
+    for (let i = 0; i < this.#sentences.length; i++) {
+      this.#sentences[i] = this.#sentences[i].trim()
+      if (this.#sentences[i] === '') {
+        this.#sentences.splice(i, 1)
+      }
+    }
+  }
+
+  /**
+   * Gets the number of sentences.
+   *
+   * @returns {number} - The number of sentences.
+   */
+  getSentenceCount() {
+    return this.#sentences.length
+  }
+
+  /**
+   * Gets the first word of each sentence and returns them in alphabetical
+   * order.
+   * 
+   * @returns {string[]} - The first word of each sentence in an array.
+   */
+  getFirstWordOfSentencesCountInAlphabeticalOrder() {
+    if (!this.#sentences) {
+      return {}
+    }
+
+    const firstWords = []
+    for (const sentence of this.#sentences) {
+      const words = sentence.match(/\b[a-zA-Z0-9-'./:]*[a-zA-Z][a-zA-Z0-9-'./:]*\b/gi)
+      if (words) {
+      firstWords.push(words[0])
+      }
+    }
+
+    const firstWordsCounterAndSorter = new CharacterCounterAndSorter(firstWords)
+    return firstWordsCounterAndSorter.getSortedCharacters()
+  }
+}
