26280549cc008a82452b81260fc6ec87960863b3
Anja Willsund <aw22hs@student.lnu.se>
2023-11-05 20:41:32 +0100

    Update row length

--- test/CharacterCounterAndSorter.test.js
+++ test/CharacterCounterAndSorter.test.js
@@ -18,18 +18,15 @@ describe('get sorted characters', () => {
     return JSON.stringify(keys) === JSON.stringify(sortedKeys);
   }
 
-  test('should return an object with one character plus character count when ' +
-    'there is one character', () => {
+  test('should return an object with one character plus character count when there is one character', () => {
     const characterCounterAndSorter = new CharacterCounterAndSorter(['A'])
-    expect(characterCounterAndSorter.getSortedCharacters())
-      .toEqual({ A: 1 })
+    expect(characterCounterAndSorter.getSortedCharacters()).toEqual({ A: 1 })
   })
 
-  test('should return an objects of words sorted in alphabetical order plus ' +
-    'word count when there are several words', () => {
-    const characterCounterAndSorter = new CharacterCounterAndSorter(['This',
-      'is', 'a', 'sentence', 'This', 'is', 'yet', 'another', 'sentence', 'And',
-      'this', 'is', 'a', 'third', 'one'])
+  test('should return an objects of words sorted in alphabetical order plus word count when there are several words',
+    () => {
+    const characterCounterAndSorter = new CharacterCounterAndSorter(['This', 'is', 'a', 'sentence', 'This', 
+      'is', 'yet', 'another', 'sentence', 'And', 'this', 'is', 'a', 'third', 'one'])
     const result = characterCounterAndSorter.getSortedCharacters()
     expect(isObjectSorted(result)).toBe(true)
   })
