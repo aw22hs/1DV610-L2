bc9fc7bbe3aa61373c2a115225683c9a3b0e7da2
Anja Willsund <aw22hs@student.lnu.se>
2023-11-05 20:13:52 +0100

    Add test for too long word

--- test/WordValidator.test.js
+++ test/WordValidator.test.js
@@ -23,29 +23,28 @@ describe('WordValidator', () => {
     })
 
     test('should throw an error for a word without any letters', () => {
-      expect(() => new WordValidator('12345')).toThrowError('The submitted ' +
-        'word does not have the right format.')
+      expect(() => new WordValidator('12345')).toThrowError('The submitted word does not have the right format.')
+    })
+
+    test('should throw an error for a word with too many letters', () => {
+      expect(() => new WordValidator('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'))
+        .toThrowError('The submitted word is too long.')
     })
 
     test('should not throw an error for a valid word', () => {
       expect(() => new WordValidator('test')).not.toThrow()
     })
 
-    test('should not throw an error for a word with numbers and special ' +
-      'characters', () => {
+    test('should not throw an error for a word with numbers and special characters', () => {
       expect(() => new WordValidator("test-1.2:3/4'5")).not.toThrow()
     })
 
-    test('should throw an error for a word without letters but with special ' +
-      'characters', () => {
-      expect(() => new WordValidator("-.:/'")).toThrowError('The submitted ' +
-        'word does not have the right format.')
+    test('should throw an error for a word without letters but with special characters', () => {
+      expect(() => new WordValidator("-.:/'")).toThrowError('The submitted word does not have the right format.')
     })
 
-    test('should throw an error for a word without letters but with special ' +
-      'characters', () => {
-      expect(() => new WordValidator('a!c')).toThrowError('The submitted ' +
-        'word does not have the right format.')
+    test('should throw an error for a word without letters but with special characters', () => {
+      expect(() => new WordValidator('a!c')).toThrowError('The submitted word does not have the right format.')
     })
   })
 })
