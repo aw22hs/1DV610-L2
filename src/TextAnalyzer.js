f0d2906af3a754ce8a28dab60664708751360f02
Anja Willsund <aw22hs@student.lnu.se>
2023-11-05 20:55:40 +0100

    Remove commentä

--- src/TextAnalyzer.js
+++ src/TextAnalyzer.js
@@ -9,8 +9,6 @@ import { CharacterCounterAndSorter } from './CharacterCounterAndSorter.js'
 import { SentenceCounter } from './SentenceCounter.js'
 import { WordCounter } from './WordCounter.js'
 
-// TODO: Ändra ordning på metoderna
-
 /**
  * Represents a card table.
  */
@@ -54,62 +52,62 @@ export class TextAnalyzer {
     return average ? average : 0
   }
 
-    /**
-     * Counts the number of characters in a text.
-     *
-     * @returns {number} - The number of characters in the text.
-     */
-    getCharacterCount() {
-      const countableCharacters = []
-      for (const character of this.#originalText) {
-        if (!character.match(/[\n]/)) {
-          countableCharacters.push(character)
-        }
+  /**
+   * Counts the number of characters in a text.
+   *
+   * @returns {number} - The number of characters in the text.
+   */
+  getCharacterCount() {
+    const countableCharacters = []
+    for (const character of this.#originalText) {
+      if (!character.match(/[\n]/)) {
+        countableCharacters.push(character)
       }
-
-      return countableCharacters.length
     }
 
-    /**
-     * Counts the number of times all different letters appear in a text.
-     * Case insensitive.
-     *
-     * @returns {object} - An object with the letters in lower case as keys and
-     * the number of times they appear as values.
-     * @throws {Error} - If there are no letters in the string.
-     */
-    getLetterCountInAlphabeticalOrder() {
-      const textInLowerCase = this.#originalText.toLowerCase()
-      const letters = textInLowerCase.match(/\p{L}/gu)
-      if (!letters) {
-        throw new Error('There are no letters in the string.')
-      }
+    return countableCharacters.length
+  }
 
-      const letterCounterAndSorter = new CharacterCounterAndSorter(letters)
-      return letterCounterAndSorter.getSortedCharacters()
+  /**
+   * Counts the number of times all different letters appear in a text.
+   * Case insensitive.
+   *
+   * @returns {object} - An object with the letters in lower case as keys and
+   * the number of times they appear as values.
+   * @throws {Error} - If there are no letters in the string.
+   */
+  getLetterCountInAlphabeticalOrder() {
+    const textInLowerCase = this.#originalText.toLowerCase()
+    const letters = textInLowerCase.match(/\p{L}/gu)
+    if (!letters) {
+      throw new Error('There are no letters in the string.')
     }
 
-    /**
-     * Counts the paragraphs in a text.
-     *
-     * @returns {number} - The number of paragraphs in the text.
-     */
-    getParagraphsCount() {
-      const paragraphs = this.#originalText.split(/\n\n/)
-      for (const paragraph of paragraphs) {
-        if (paragraph === '') {
-          paragraphs.splice(paragraphs.indexOf(paragraph), 1)
-        }
-      }
+    const letterCounterAndSorter = new CharacterCounterAndSorter(letters)
+    return letterCounterAndSorter.getSortedCharacters()
+  }
 
-      return paragraphs.length
+  /**
+   * Counts the paragraphs in a text.
+   *
+   * @returns {number} - The number of paragraphs in the text.
+   */
+  getParagraphsCount() {
+    const paragraphs = this.#originalText.split(/\n\n/)
+    for (const paragraph of paragraphs) {
+      if (paragraph === '') {
+        paragraphs.splice(paragraphs.indexOf(paragraph), 1)
+      }
     }
 
-    #validateTextInput(text) {
-      if (!text) {
-        throw new Error('There are no characters in the string.')
-      } else if (this.getCharacterCount() > 10000) {
-        throw new Error('There are more than 10 000 characters in the string.')
-      }
+    return paragraphs.length
+  }
+
+  #validateTextInput(text) {
+    if (!text) {
+      throw new Error('There are no characters in the string.')
+    } else if (this.getCharacterCount() > 10000) {
+      throw new Error('There are more than 10 000 characters in the string.')
     }
   }
+}
